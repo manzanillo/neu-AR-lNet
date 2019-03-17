95d720384edfeafae4c6ec00df1cf4e2216aacc7
Max Hänsel <mh@maxhaensel.de>
2019-03-17 19:50:54 +0100

    hello World

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -6,92 +6,174 @@ import { StyleSheet } from 'react-native'
 
 import {
   ViroARScene,
-  ViroText,
   ViroMaterials,
   ViroSphere,
   ViroAmbientLight,
   ViroSpotLight,
-  ViroAnimations
+  ViroARPlaneSelector,
+  ViroAnimations,
+  ViroBox
 } from 'react-viro'
+import { Z_FULL_FLUSH } from 'zlib'
 
-const input = [[-0.5, 0.25, -2], [-0.5, -0.25, -2]]
-const hidden = [[0, 0.35, -2], [0, 0, -2], [0, -0.35, -2]]
-const output = [[0.5, 0.25, -2], [0.5, -0.25, -2]]
+const distance = -0.5
+
+const input = [[-0.15, 0, 0.15], [-0.15, 0, -0.15]]
+const hidden = [[0, 0, 0.2], [0, 0, 0], [0, 0, -0.2]]
+const output = [[0.15, 0, 0.15], [0.15, 0, -0.15]]
+
+const streams = []
+const streamsRef = []
+const animation = {}
+input.map(inputV => {
+  hidden.map(hiddenV => {
+    calculate(inputV, hiddenV)
+  })
+})
+
+hidden.map(inputV => {
+  output.map(hiddenV => {
+    calculate(inputV, hiddenV)
+  })
+})
 
 export default class HelloWorldScene extends Component {
   constructor() {
     super()
 
-    this.state = {} // Set initial state here
+    this.state = {
+      material: 'red_sphere'
+    } // Set initial state here
   }
 
   staticViroSphere = input.concat(hidden).concat(output)
+  layers = [
+    { material: 'blue_sphere', pos: input },
+    { material: 'lightblue_sphere', pos: hidden },
+    { material: 'blue_sphere', pos: output }
+  ]
 
-  streams = [[-0.5, 0.25, -2], [-0.5, 0.25, -2], [-0.5, 0.25, -2]]
-  // [0, 0.35, -2], [0, 0, -2], [0, -0.35, -2]
+  _setARNodeRef(component) {
+    if (!this.arNodeRef) {
+      this.arNodeRef = [component]
+    } else {
+      this.arNodeRef.push(component)
+    }
+  }
+
+  changecolor() {
+    streamsRef[0].setNativeProps({ materials: ['red_sphere'] })
+    streamsRef[1].setNativeProps({ materials: ['red_sphere'] })
+    streamsRef[2].setNativeProps({ materials: ['red_sphere'] })
+  }
 
   render() {
     return (
       <ViroARScene>
-        <ViroSpotLight
-          position={[0, 0, 3]}
-          color="#777777"
-          direction={[0, 0, -1]}
-          attenuationStartDistance={5}
-          attenuationEndDistance={10}
-          innerAngle={5}
-          outerAngle={40}
-        />
-        <ViroAmbientLight color="#eee" />
-
-        {this.staticViroSphere.map(pos => (
-          <ViroSphere
-            key={Math.random()}
-            heightSegmentCount={20}
-            widthSegmentCount={20}
-            radius={0.1}
-            position={pos}
-            materials={['blue_sphere']}
+        <ViroARPlaneSelector minHeight={0.5} minWidth={0.5}>
+          <ViroSpotLight
+            position={[0, 0, 3]}
+            color="#777777"
+            direction={[0, 0, -1]}
+            attenuationStartDistance={5}
+            attenuationEndDistance={10}
+            innerAngle={5}
+            outerAngle={40}
           />
-        ))}
+          <ViroAmbientLight color="#eee" />
 
-        {
-          // Animation
-        }
-        <ViroSphere
-          heightSegmentCount={20}
-          widthSegmentCount={20}
-          radius={0.05}
-          visible={true}
-          position={[-0.5, 0.25, -2]}
-          materials={['red_sphere']}
-          animation={{ name: 'pack', run: true, loop: true }}
-        />
+          {this.staticViroSphere.map(pos => (
+            <ViroSphere
+              onClick={this.changecolor.bind(this)}
+              key={Math.random()}
+              heightSegmentCount={20}
+              widthSegmentCount={20}
+              radius={0.025}
+              position={pos}
+              materials={['blue_sphere']}
+              ref={this._setARNodeRef.bind(this)}
+            />
+          ))}
+          {streams.map(pos => pos)}
+        </ViroARPlaneSelector>
       </ViroARScene>
     )
   }
 }
 
-animation = {}
+function pushRef(component) {
+  streamsRef.push(component)
+}
 
-input.map(inputV => {
-  hidden.map(hiddenV => {
-    animation
-  })
-})
+function calculate(inputV, hiddenV) {
+  for (let i = 0; i <= 0; i++) {
+    const animationName = Math.random()
+      .toString(36)
+      .substring(7)
 
-const animation = {
-  moveRight: {
-    properties: { positionX: '+=0.5', positionY: '+=0.1' },
-    easing: 'Linear',
-    duration: 2000
-  },
-  moveLeft: {
-    properties: { positionX: '-=0.5', positionY: '-=0.1' },
-    easing: 'Linear',
-    duration: 0
-  },
-  pack: [['moveRight', 'moveLeft']]
+    const element = (
+      <ViroSphere
+        key={Math.random()}
+        heightSegmentCount={20}
+        widthSegmentCount={20}
+        radius={0.015}
+        position={inputV}
+        materials={['grey_sphere']}
+        animation={{ name: animationName, run: true, loop: true }}
+        ref={pushRef}
+      />
+    )
+    streams.push(element)
+
+    const x = Math.random()
+      .toString(36)
+      .substring(7)
+    const y = Math.random()
+      .toString(36)
+      .substring(7)
+
+    const deltaX = hiddenV[0] - inputV[0]
+    const deltaY = hiddenV[1] - inputV[1]
+    const deltaZ = hiddenV[2] - inputV[2]
+
+    const animation1X =
+      deltaX > 0 ? `+=${Math.abs(deltaX)}` : `-=${Math.abs(deltaX)}`
+    const animation1Y =
+      deltaY > 0 ? `+=${Math.abs(deltaY)}` : `-=${Math.abs(deltaY)}`
+    const animation1Z =
+      deltaZ > 0 ? `+=${Math.abs(deltaZ)}` : `-=${Math.abs(deltaZ)}`
+
+    const animation2X =
+      deltaX > 0 ? `-=${Math.abs(deltaX)}` : `+=${Math.abs(deltaX)}`
+    const animation2Y =
+      deltaY > 0 ? `-=${Math.abs(deltaY)}` : `+=${Math.abs(deltaY)}`
+    const animation2Z =
+      deltaZ > 0 ? `-=${Math.abs(deltaZ)}` : `+=${Math.abs(deltaZ)}`
+
+    animation[x] = {
+      properties: {
+        positionX: animation1X,
+        positionY: animation1Y,
+        positionZ: animation1Z
+      },
+      easing: 'Linear',
+      duration: Math.round(Math.random() * (3000 - 2000)) + 2000,
+      delay:
+        i === 1
+          ? Math.round(Math.random() * (150 - 0)) + 0
+          : Math.round(Math.random() * (150 - 0)) + 1000
+    }
+    animation[y] = {
+      properties: {
+        positionX: animation2X,
+        positionY: animation2Y,
+        positionZ: animation2Z
+      },
+      easing: 'Linear',
+      duration: 0
+    }
+    animation[animationName] = [[x, y]]
+  }
 }
 
 ViroAnimations.registerAnimations(animation)
@@ -110,6 +192,10 @@ ViroMaterials.createMaterials({
   blue_sphere: {
     lightingModel: 'PBR',
     diffuseColor: 'rgb(19,42,143)'
+  },
+  lightblue_sphere: {
+    lightingModel: 'PBR',
+    diffuseColor: 'rgb(135,206,250)'
   },
   grey_sphere: {
     lightingModel: 'PBR',
